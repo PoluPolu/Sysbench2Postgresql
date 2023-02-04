4b47475fa9530fc12dd55429a46528ce592ac85c
PoluPolu <ppproko@gmail.com>
2023-02-05 00:04:00 +0100

    new file

--- SYSBENCH.sql
+++ SYSBENCH.sql
@@ -3,23 +3,27 @@ drop table sysbench ;
 CREATE TABLE "sysbench" (
 	"id" SERIAL,
 	"file_name" VARCHAR(100),
-	"total_time" numeric (40,20),
-	"total_events" numeric (40,20),
-	"latency_min" numeric (40,20),
-	"latency_avg" numeric (40,20),
-	"latency_max" numeric (40,20),
-	"p95" numeric (40,20),
-	"latency_sum" numeric (40,20),
-	"threads_events_avg" numeric (40,20),
-	"threads_events_stddev" numeric (40,20),
-	"threads_exectime_avg" numeric (40,20),
-	"threads_exectime_stddev" numeric (40,20),
+	"cpu_speed_events_per_second" numeric (40,10),
+	"gen_total_time" numeric (40,10),
+	"gen_total_numb_events" numeric (40,10),
+	"latency_ms_min" numeric (40,10),
+	"latency_ms_avg" numeric (40,10),
+	"latency_ms_max" numeric (40,10),
+	"p95" numeric (40,10),
+	"latency_ms_sum" numeric (40,10),
+	"threads_events_avg" numeric (40,10),
+	"threads_events_stddev" numeric (40,10),
+	"threads_exectime_avg" numeric (40,10),
+	"threads_exectime_stddev" numeric (40,10),
 	"controller" VARCHAR(100),
 	"sysbench" numeric (40,20),
 	"test_type" VARCHAR(100),
 	"test_kind" VARCHAR(100),
 	PRIMARY KEY ("id")
 );
+
+
+
 
 alter table sysbench 
 alter column total_time type numeric (40,20),
@@ -32,7 +36,8 @@ threads_events_avg, threads_events_stddev, threads_exectime_avg, threads_exectim
 VALUES('a', 10.0005, 10.1, 11.0002, 0, 0, 0, 0, 0, 0, 0, 0);
 
 select  
-controller, sysbench, test_type, test_kind, *
+--controller, sysbench, test_type, test_kind, 
+*
 from sysbench s 
 ;
 
@@ -44,3 +49,8 @@ order by 1
 
 
 truncate sysbench ; 
+
+select substring(file_name,1,16), count(*)
+from sysbench s
+group by substring(file_name,1,16) 
+;
